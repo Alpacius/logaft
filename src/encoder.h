530132506df16c57cc55c6c118b6eccec0977750
Akvelog <sceneviper@hotmail.com>
2018-05-11 22:39:04 +0900

    encoder control block.

--- src/encoder.h
+++ src/encoder.h
@@ -2,5 +2,14 @@
 
 #include    "std_common.h"
 #include    "linux_common.h"
+#include    "miscutils.h"
+#include    "mbuf.h"
 
+struct laft_encoder {
+    struct mbuf *log_encode(struct laft_encoder *, const char *);
+};
 
+#define     encoder                 struct laft_encoder encoder_ctl_
+#define     encoder_from_imm(i_)    (&((i_).encoder_ctl_))
+#define     encoder_from_ptr(p_)    (&((p_)->encoder_ctl_))
+#define     encoder_impl(i_, t_)    container_of((i_), t_, encoder_ctl_)
