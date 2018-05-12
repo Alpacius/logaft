35a31ccfbc9b5f35c57b59380720bcec521cb7cb
Akvelog <sceneviper@hotmail.com>
2018-05-12 19:14:20 +0900

    tqueue opt.

--- src/tqueue.h
+++ src/tqueue.h
@@ -12,39 +12,23 @@ struct tqueue {
 
 static inline
 int tqueue_init(struct tqueue *tq, uint32_t size) {
-    tq->size = size;
-    if (unlikely((tq->evfds = malloc(sizeof(int) * size)) == NULL))
-        return 0;
-    for (uint32_t i = 0; i < size; i++)
-        if ((tq->evfds[i] = eventfd(0, EFD_CLOEXEC)) == -1) {
-            for (uint32_t j = 0; j < i; j++)
-                close(tq->evfds[j]);
-            return free(tq->evfds), 0;
-        }
-    if (unlikely((tq->q = pqueue_new(size)) == NULL)) {
-        for (uint32_t j = 0; j < size; j++)
-            close(tq->evfds[j]);
-        return 0;
-    }
+    // TODO implementation
     return 1;
 }
 
 static inline
 void tqueue_ruin(struct tqueue *tq) {
-    for (uint32_t i = 0; i < tq->size; i++)
-        close(tq->evfds[i]);
     pqueue_delete(tq->q);
 }
 
 static inline
-int tqueue_dump(struct tqueue *tq, uint32_t tok, struct link_index *t) {
-    uint64_t sink;
-    return (read(tq->evfds[tok], &sink, sizeof(sink)) > -1) ?
-        pqueue_dump(tq->q, tok, t) :
-        0;
+struct link_index *tqueue_shift(struct tqueue *tq, uint32_t tok) {
+    // TODO implementation
+    return NULL;
 }
 
 static inline
 void tqueue_push_back(struct tqueue *tq, struct link_index *elt) {
     pqueue_push_back(tq->q, elt);
+    // TODO implementation
 } 
