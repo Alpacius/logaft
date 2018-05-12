df19a6f3a2d8d8b376578751a54c32e3e7109fa8
Akvelog <sceneviper@hotmail.com>
2018-05-12 16:11:46 +0900

    encoder & writer control block.

--- src/encoder.h
+++ src/encoder.h
@@ -6,10 +6,10 @@
 #include    "mbuf.h"
 
 struct laft_encoder {
-    struct mbuf *log_encode(struct laft_encoder *, const char *);
+    struct mbuf (*log_encode)(struct laft_encoder *, const char *);
 };
 
 #define     encoder                 struct laft_encoder encoder_ctl_
 #define     encoder_from_imm(i_)    (&((i_).encoder_ctl_))
 #define     encoder_from_ptr(p_)    (&((p_)->encoder_ctl_))
-#define     encoder_impl(i_, t_)    container_of((i_), t_, encoder_ctl_)
+#define     encoder_impl(p_, t_)    container_of((p_), t_, encoder_ctl_)
---
+++ src/writer.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include    "std_common.h"
+#include    "linux_common.h"
+#include    "miscutils.h"
+#include    "mbuf.h"
+
+struct laft_writer {
+    int (*log_write)(struct laft_writer *, struct mbuf *);
+};
+
+#define     writer                  struct laft_writer writer_ctl_
+#define     writer_from_imm(i_)     (&((i_).writer_ctl_))
+#define     writer_from_ptr(p_)     (&((p_)->writer_ctl_))
+#define     writer_impl(p_, t_)     container_of((p_), t_, writer_ctl_)
