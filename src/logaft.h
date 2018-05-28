fa3c67917b09433a1aae4f40aa9438c9a71419bc
Akvelog <sceneviper@hotmail.com>
2018-05-29 00:01:04 +0900

    refactor & add build scripts.

--- src/logaft.h
+++ src/logaft.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include    "logger.h"
-#include    "singletons.h"
 #include    "simple_encoder.h"
 #include    "simple_writer.h"
+
+int laft_init(void);
+int laft_ruin(void);
--- src/logger.c
+++ src/logger.c
@@ -1,4 +1,5 @@
 #include    "logger.h"
+#include    "singletons.h"
 
 
 static uint32_t id_cnt = 0; 
